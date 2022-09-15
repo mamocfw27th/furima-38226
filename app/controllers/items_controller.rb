c1c9d02fc9c0c56926fe46e80ac12ee8c3cdf12f
mamocfw27th <108714748+mamocfw27th@users.noreply.github.com>
2022-09-15 17:24:08 +0900

    ルーティング、アクション、削除ボタンのパス設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, except: [:index, :create, :new]
   before_action :move_to_new, only: :edit
 
   def index
@@ -35,6 +35,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+  end
 
   private
 
