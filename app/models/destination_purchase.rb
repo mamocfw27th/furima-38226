19a15f733835c6cf90fd63ab58500679fb0ef621
mamocfw27th <108714748+mamocfw27th@users.noreply.github.com>
2022-09-21 15:07:11 +0900

    必要なモデル等の作成

--- app/models/destination_purchase.rb
+++ app/models/destination_purchase.rb
@@ -1,13 +1,14 @@
 class DestinationPurchase
   include ActiveModel::Model
-  attr_accessor :post_num, :region_id, :city, :address, :building, :phone, :purchase, :user, :item 
+  attr_accessor :post_num, :region_id, :city, :address, :building, :phone, :purchase, :user_id, :item_id 
 
   with_options presence: ture do 
     validates :post_num, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :region_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :phone, length: { minimum: 10, maximum: 11 }, numericality: format: {with: /\A[0-9]\/z, message: "can't be blank"}
     validates :address, :city
-    validates :user_id
+    validates :user_id, :item_id
+    validates :token
   end
 
   def save
